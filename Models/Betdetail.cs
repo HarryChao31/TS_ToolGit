fc00155bbc35b596a6304048c47371793bb91ca6
Harry Chao <harry.chao@remotes.local>
2024-03-27 00:11:12 +0800

    Init commit

---
+++ Models/Betdetail.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using TS_Tool.Models;
+namespace TS_Tool.Models
+{
+    public class Betdetail
+    {
+        [Key]
+        public int Webid { get; set; }
+        public string Refno { get; set; }
+        public string UserName { get; set; }
+        [Column(TypeName = "Bigint")]
+        public long Transid { get; set; }
+        public string Match { get; set; }
+        public string League { get; set; }
+        public int MatchResultId { get; set;}
+        public string Status { get; set; }
+        public string OsStatus { get; set; }
+        [Column(TypeName = "tinyint")]
+        public Byte BetType { get; set; }
+        public string BetOption { get; set; }
+        public string Remark { get; set;}
+       //SW Error
+       // public string? Action { get; set;}
+        //public string? ErrorMessage { get; set; }
+        //public string? HostName { get; set; }
+        //public string? Request { get; set; }
+    }
+}
